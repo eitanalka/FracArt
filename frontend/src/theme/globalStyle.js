dcfdcb6fc698583ca8630a449406d4666aaa1c5d
Eitan Alkaslassy <eitanalka@gmail.com>
2019-04-09 19:34:29 -0400

    added styled-components based on https://medium.com/styled-components/styled-components-getting-started-c9818acbcbbd

---
+++ frontend/src/theme/globalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Lato', sans-serif;
+  }
+`
+
+export default GlobalStyle;
