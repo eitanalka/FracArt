ac94d5ba9bc7c2f7422b61ddcee356b069372e29
Eitan Alkaslassy <eitanalka@gmail.com>
2019-04-09 20:52:42 -0400

    created router and initial home component

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,67 +1,22 @@
 import React, { Component } from 'react';
-import styled, { keyframes } from 'styled-components';
+import { Router, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
 import GlobalStyle from '../theme/globalStyle';
+import Home from './home';
 
-const AppWrapper = styled.div`
-  text-align: center;
-`;
+export const history = createBrowserHistory();
 
-const rotate360 = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const AppLogo = styled.img`
-  animation: ${rotate360} infinite 120s linear;
-  height: 80px;
-  &:hover {
-    animation: ${rotate360} infinite 1.5s linear;
-  }
-`;
-
-const AppHeader = styled.div`
-  background-color: #222;
-  height: 12rem;
-  padding: 1rem;
-  color: white;
-`;
-
-const AppTitle = styled.h1`
-  font-weight: 900;
-`;
-
-const AppIntro = styled.p`
-  font-size: large;
-  code {
-    font-size: 1.3rem;
-  }
-`;
-
-const EmojiWrapper = styled.span.attrs({
-  role: 'img'
-})``;
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <GlobalStyle />
-        <AppWrapper>
-          <AppHeader>
-            <AppTitle>Welcome to React</AppTitle>
-          </AppHeader>
-          <AppIntro>
-            Bootstrapped with <code>create-react-app</code>.
-          </AppIntro>
-          <AppIntro>
-            Components styled with <code>styled-components</code>{' '}
-            {/* eslint-disable-next-line */}
-            <EmojiWrapper aria-label="nail polish">💅</EmojiWrapper>
-          </AppIntro>
-        </AppWrapper>
+        <Router history={history}>
+          <div>
+            <Route exact path="/" component={Home} />
+          </div>
+        </Router>
       </React.Fragment>
     );
   }
---
+++ frontend/src/app/home/HomeComponent.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HomeComponent = () => <div>Home Component</div>;
+
+export default HomeComponent;
