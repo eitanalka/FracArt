6f974560d32fccf89a257becdef1078c4983a5c2
Eitan Alkaslassy <eitanalka@gmail.com>
2019-04-09 20:44:08 -0400

    changed project structure + added more depend.

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import styled, { keyframes } from 'styled-components';
-import logo from './logo.svg';
+import GlobalStyle from '../theme/globalStyle';
 
 const AppWrapper = styled.div`
   text-align: center;
-`
+`;
 
 const rotate360 = keyframes`
   from {
@@ -13,7 +13,7 @@ const rotate360 = keyframes`
   to {
     transform: rotate(360deg);
   }
-`
+`;
 
 const AppLogo = styled.img`
   animation: ${rotate360} infinite 120s linear;
@@ -21,47 +21,49 @@ const AppLogo = styled.img`
   &:hover {
     animation: ${rotate360} infinite 1.5s linear;
   }
-`
+`;
 
 const AppHeader = styled.div`
   background-color: #222;
   height: 12rem;
   padding: 1rem;
   color: white;
-`
+`;
 
 const AppTitle = styled.h1`
   font-weight: 900;
-`
+`;
 
 const AppIntro = styled.p`
   font-size: large;
   code {
     font-size: 1.3rem;
   }
-`
+`;
 
 const EmojiWrapper = styled.span.attrs({
   role: 'img'
-})``
+})``;
 class App extends Component {
   render() {
     return (
-      <AppWrapper>
-        <AppHeader>
-          <AppLogo src={logo} alt="logo" />
-          <AppTitle>Welcome to React</AppTitle>
-        </AppHeader>
-        <AppIntro>
-          Bootstrapped with <code>create-react-app</code>.
-        </AppIntro>
-        <AppIntro>
-          Components styled with <code>styled-components</code>{' '}
-          {/* eslint-disable-next-line */}
-          <EmojiWrapper aria-label="nail polish">💅</EmojiWrapper>
-        </AppIntro>
-      </AppWrapper>
-    )
+      <React.Fragment>
+        <GlobalStyle />
+        <AppWrapper>
+          <AppHeader>
+            <AppTitle>Welcome to React</AppTitle>
+          </AppHeader>
+          <AppIntro>
+            Bootstrapped with <code>create-react-app</code>.
+          </AppIntro>
+          <AppIntro>
+            Components styled with <code>styled-components</code>{' '}
+            {/* eslint-disable-next-line */}
+            <EmojiWrapper aria-label="nail polish">💅</EmojiWrapper>
+          </AppIntro>
+        </AppWrapper>
+      </React.Fragment>
+    );
   }
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
-import GlobalStyle from './theme/globalStyle';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
 
-const Root = () => (
-  <div>
-    <GlobalStyle />
-    <App />
-  </div>
-)
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
