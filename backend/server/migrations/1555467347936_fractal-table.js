bf507050ed0f475653aca5535b3d6cb8ce668d72
Eitan Alkaslassy <eitanalka@gmail.com>
2019-04-16 22:29:26 -0400

    created schema for fractal table and validate title is at most 50 characters

---
+++ backend/server/migrations/1555467347936_fractal-table.js
@@ -0,0 +1,35 @@
+exports.up = pgm => {
+  pgm.createTable('fractal', {
+    id: 'id',
+    title: {
+      type: 'varchar(50)',
+      notNull: true
+    },
+    settings: {
+      type: 'json',
+      notNull: true
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp')
+    },
+    updated_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp')
+    }
+  });
+
+  // trigger to set updatedAt to current time when a fractal is updated
+  pgm.createTrigger('fractal', 'set_timestamp', {
+    when: 'BEFORE',
+    operation: 'UPDATE',
+    function: 'trigger_set_timestamp',
+    level: 'ROW'
+  });
+};
+
+exports.down = pgm => {
+  pgm.dropTable('fractal');
+};
